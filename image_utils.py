82b98e119faf01f261e37d8c1c3365405c45f21b
Vlad Levochko <levochkovs@gmail.com>
2017-11-17 10:49:23 +0200

    changes in squeezenet runner

--- image_utils.py
+++ image_utils.py
@@ -1,56 +1,10 @@
 from __future__ import print_function
-from future import standard_library
-standard_library.install_aliases()
-from builtins import range
 import urllib.request, urllib.error, urllib.parse, os, tempfile
-
-import numpy as np
+from future import standard_library
 from scipy.misc import imread, imresize
+import numpy as np
 
-"""
-Utility functions used for viewing and processing images.
-"""
-
-def blur_image(X):
-    """
-    A very gentle image blurring operation, to be used as a regularizer for
-    image generation.
-
-    Inputs:
-    - X: Image data of shape (N, 3, H, W)
-
-    Returns:
-    - X_blur: Blurred version of X, of shape (N, 3, H, W)
-    """
-    from cs231n.fast_layers import conv_forward_fast
-    w_blur = np.zeros((3, 3, 3, 3))
-    b_blur = np.zeros(3)
-    blur_param = {'stride': 1, 'pad': 1}
-    for i in range(3):
-        w_blur[i, i] = np.asarray([[1, 2, 1], [2, 188, 2], [1, 2, 1]],
-                                  dtype=np.float32)
-    w_blur /= 200.0
-    return conv_forward_fast(X, w_blur, b_blur, blur_param)[0]
-
-
-SQUEEZENET_MEAN = np.array([0.485, 0.456, 0.406], dtype=np.float32)
-SQUEEZENET_STD = np.array([0.229, 0.224, 0.225], dtype=np.float32)
-
-def preprocess_image(img):
-    """Preprocess an image for squeezenet.
-    
-    Subtracts the pixel mean and divides by the standard deviation.
-    """
-    return (img.astype(np.float32)/255.0 - SQUEEZENET_MEAN) / SQUEEZENET_STD
-
-
-def deprocess_image(img, rescale=False):
-    """Undo preprocessing on an image and convert back to uint8."""
-    img = (img * SQUEEZENET_STD + SQUEEZENET_MEAN)
-    if rescale:
-        vmin, vmax = img.min(), img.max()
-        img = (img - vmin) / (vmax - vmin)
-    return np.clip(255 * img, 0.0, 255.0).astype(np.uint8)
+# standard_library.install_aliases()
 
 
 def image_from_url(url):
--- runner_squeezenet.py
+++ runner_squeezenet.py
@@ -1,32 +1,50 @@
-import cs231n.image_utils as image_utils
-import cs231n.classifiers.squeezenet as squeezenet
 import tensorflow as tf
-import squeezenet_helper
-import matplotlib.pyplot as plt
-from app import App
+from scipy.misc import imsave
 
+import model.squeezenet as squeezenet
+import squeezenet_helper
+from image_utils import load_image
+from app import App
 
 if __name__ == '__main__':
     SAVE_PATH = 'cs231n/datasets/squeezenet.ckpt'
 
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument("--content_image", required=True)
+    # parser.add_argument("--content_image_size", default=None)
+    # parser.add_argument("--style_image", required=True)
+    # parser.add_argument("--style_image_size", default=None)
+    # parser.add_argument("--output_image", required=True)
+    #
+    # args = parser.parse_args()
+    # content_image_path = args.content_image
+    # content_image_size = args.content_image_size
+    # style_image_path = args.style_image
+    # style_image_size = args.style_image_size
+    # output_path = args.output_image
+
     config = {
         'init_random': True,
-        'learning_rate': 9e-1,
-        'decayed_lr': 4e-1,
-        'decay_at': 100,
+        'decaying_lr': True,
+        'learning_rate': 1e-1,
+        'decay_at': [500],
+        'lr_values': [1e-2],
+
         'num_iterations': 500,
-        # 'content_layers': [3],
-        'content_layers': [1, 2, 3],
-        # 'style_layers': [1, 3,  4, 6],
-        'style_layers': [1, 2, 3, 4, 6, 7, 9],
-        'content_weights': [7cs , 7, 7],
-        # 'style_weights': [256, 128, 2, 1],
-        'style_weights': [21, 21, 1, 1, 1, 7, 7],
-        'tv_weight': 0,  # 1e-2
+        'content_layers': [3],
+        'style_layers': [1, 3, 4, 6],  # , 7, 9],
+        'style_weights': [1, 1, 1, 1, 1, 1],  # [21 * 100, 50, 5, 5, 7, 7],
+        'content_weight': 0,  # 1e-3,
+        'style_weight': 1,
+        # 'tv_weight': 0,  # 1e-2
         'summary_directory': 'summary/squeezenet'
     }
-    content_image = image_utils.load_image("styles/tubingen.jpg", size=256)
-    style_image = image_utils.load_image("styles/starry_night.jpg", size=512)
+
+    # content_image = image_utils.load_image(content_image_path, size=content_image_size)
+    # style_image = image_utils.load_image(style_image_path, size=style_image_size)
+
+    content_image = load_image("styles/blank.jpg", size=512)
+    style_image = load_image("styles/the_scream.jpg")
 
     session = tf.Session()
 
@@ -36,14 +54,5 @@ if __name__ == '__main__':
 
     generated_image = app.style_transfer(content_image, style_image)
 
-    f, axarr = plt.subplots(1, 3)
-    axarr[0].axis('off')
-    axarr[1].axis('off')
-    axarr[2].axis('off')
-    axarr[0].set_title('Content Source Img.')
-    axarr[1].set_title('Style Source Img.')
-    axarr[2].set_title('Stylized Image')
-    axarr[0].imshow(content_image)
-    axarr[1].imshow(style_image)
-    axarr[2].imshow(generated_image)
-    plt.show()
+    # imsave(output_path, generated_image)
+    imsave("generated/style_1346_w1_s512.png", generated_image)
